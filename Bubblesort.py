fce5490efc22139ccde948c932bd7a9148b6c6e2
Peter Macdonald <peter_macdonald@live.com>
2018-11-15 16:27:33 +0100

    Update Bubblesort.py
    Fixed indentation on line 8

--- Bubblesort.py
+++ Bubblesort.py
@@ -5,7 +5,7 @@ def bubbleSort(myList):
 	    for o in range (0, len(myList) - 1 - i):
 		    if myList[o] > myList[o+1]:
 			    myList[o], myList[o+1] = myList[o+1], myList[o]
-	return myList
+    return myList
 	
 theList = [4, 2, 3, 5, 7, 2 ,4 ,1]
 print (bubbleSort (theList))
